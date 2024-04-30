a0942fd0ece262381c8f491ab70f537a6502ea86
Piyush Jain <=>
2024-04-30 08:04:36 +0530

    code solution   medium array problmes like kadane algo, hashing ,sliding window

--- Solutions/src/arrays/MediumArrayProblems.java
+++ Solutions/src/arrays/MediumArrayProblems.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,6 +12,10 @@ public class MediumArrayProblems {
         int longestLength = longestSubStringWithoutRepeatingCharacters(str);
         System.out.println("Length of the  longest substring without repeating characters: " + longestLength);
 
+        int[] v = {2 ,2 ,1, 3, 1 ,1 ,3 ,1 ,1};
+       int n =  majorityElement(v);
+        System.out.println(n);
+
     }
 
 
@@ -20,14 +25,38 @@ public class MediumArrayProblems {
         int maxSum = 0;
         int windowSum = 0;
         for (int i = 0; i < k; i++) {
-        windowSum += nums[i];
+            windowSum += nums[i];
         }
         maxSum = windowSum;
         for (int i = k; i < nums.length; i++) {
-           windowSum += nums[i] - nums[i - k];
-           maxSum = Math.max(maxSum, windowSum);
+            windowSum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, windowSum);
         }
         return maxSum;
+    }
+
+    /*TODO You are given an array 'arr' of length 'n', consisting of integers.
+        A subarray is a contiguous segment of an array. In other words, a subarray can be formed by removing 0 or
+        more integers from the beginning and 0 or more integers from the end of an array.
+        Find the sum of the subarray (including empty subarray) having maximum sum among all subarrays.
+        The sum of an empty subarray is 0.*/
+
+    public static long sumOfMaxSubArray(int[] arr,int n){
+        long sum = 0;
+        long maxSum =  Long.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        if(maxSum<0){
+            maxSum = 0;
+        }
+            return maxSum;
     }
 
     /*TODO Longest Substring Without Repeating Characters:*/
@@ -44,7 +73,7 @@ public class MediumArrayProblems {
                 set.add(c);
                 end++;
                 maxLen = Math.max(maxLen, end - start);
-            }else{
+            } else {
                 set.remove(s.charAt(start));
                 start++;
             }
@@ -54,32 +83,120 @@ public class MediumArrayProblems {
 
     /*TODO : longest subarray with sum k includes positive numbers only*/
 
-    public static int lengthOfLongestSubArrayWithSumK(int[] a ,int k){
+    public static int lengthOfLongestSubArrayWithSumK(int[] a, int k) {
+        int n = a.length;
+        int sum = 0;
+        int maxLen = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+            int rem = sum - k;
+            if (map.containsKey(rem)) {
+                int len = i - map.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
 
-       int n = a.length;
-       int sum =0;
-       int maxLen =0;
-       Map<Integer,Integer> map = new HashMap<>();
-       for(int i =0;i<n;i++){
+    /*TODO : Check if a pair with given sum exists in Array*/
+    public static Boolean isTwoSumPresent(int[] n, int k) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n.length; i++) {
+            int num = n[i];
+            int sumNeeded = k - num;
+            if (map.containsKey(sumNeeded)) {
+                return true;
+            }
+            map.put(n[i], i);
+        }
+        return false;
+    }
 
-           sum+=a[i];
+    /*TODO : Check if a pair with given sum exists in Array*/
 
-           if(sum ==k){
-               maxLen = Math.max(maxLen, i + 1);
-           }
-           int rem = sum-k;
-           if(map.containsKey(rem)){
-               int len = i - map.get(rem);
-              maxLen = Math.max(maxLen, len);
-           }
+    public static Boolean twoSumUsingTwoPointers(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        Arrays.sort(nums);
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return true;
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
 
-           if (!map.containsKey(sum)) {
-               map.put(sum, i);
-           }
+        }
+        return false;
+    }
 
-       }
-return maxLen;
+    /*TODO You have been given an array/list 'arr' consisting of 'n' elements.
+        Each element in the array is either 0, 1 or 2.
+            Sort this array/list in increasing order.
+            Do not make a new array/list. Make changes in the given array/list.
+            Example :
+            Input: 'arr' = [2, 2, 2, 2, 0, 0, 1, 0]
+            Output: Final 'arr' = [0, 0, 0, 1, 2, 2, 2, 2]
+            */
 
+    public static void threePointers(int[] nums) {
+       Arrays.sort(nums);
+    }
+
+    /*TODO You are given an array 'a' of 'n' integers.
+       A majority element in the array ‘a’ is an element that appears more than 'n' / 2 times.
+       Find the majority element of the array.
+       It is guaranteed that the array 'a' always has a majority element.*/
+
+    public static int majorityElement(int[] num,int n){
+        int len=num.length/2;
+        Map<Integer,Integer> map=new HashMap<>();
+        int maxLength=0;
+        int currLen =0;
+        for(int i=0;i<num.length;i++){
+            if(map.containsKey(num[i])){
+                maxLength=Math.max(maxLength,map.get(num[i]));
+            }
+            if(maxLength>=len){
+                return num[i];
+            }
+            if(!map.containsKey(num[i])){
+                map.put(num[i],map.getOrDefault(num[i],0)+1);
+            }
+
+        }
+        return -1;
+    }
+
+    public static int majorityElement(int []v) {
+        // Write your code here
+        int len=v.length/2;
+        Map<Integer,Integer> map = new HashMap<>();
+        int maxLength=0;
+        for(int i=0;i<v.length;i++){
+            if(map.containsKey(v[i])){
+                map.put(v[i],map.get(v[i])+1);
+                maxLength=Math.max(maxLength,map.get(v[i]));
+            }
+            if(maxLength>=len){
+                return v[i];
+            }
+            if(!map.containsKey(v[i])){
+                map.put(v[i],map.getOrDefault(v[i],0)+1);
+            }
+
+        }
+        return -1;
     }
 
 }
